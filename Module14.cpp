0d4757ae360d85f9f8dc8fd83cd8843c4a6d1468
aleksandra_orlova <aleksa.orlik2000@gmail.com>
2023-11-17 12:51:48 +0300

    Добавьте файлы проекта.

---
+++ Module14.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1;
+	getline(cin, str1);
+	int lastCharIndex = str1.length() - 1;
+
+	cout << "Current string: " << str1 << "\n";
+	cout << "String's length: " << str1.length() << "\n";
+	cout << "First symbol: " << str1.substr(0, 1) << "\n";
+	cout << "Last symbol: " << str1.substr(lastCharIndex, 1) << "\n";
+}
+
